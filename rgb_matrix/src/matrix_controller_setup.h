37d8c59f7839f5b28206b8fea64f8cd9d7410f4a
bcoburn0618 <beaucoburn2003@gmail.com>
2025-04-29 21:13:27 -0600

    Abandoning IR Build

--- rgb_matrix/src/matrix_controller_setup.h
+++ rgb_matrix/src/matrix_controller_setup.h
@@ -19,17 +19,19 @@
 #define LAT  BIT0
 #define CLK  BIT1
 #define OE   BIT2
-
+//--------------------IR Receiver----------------------
+#define IR   BIT3
 
 #define SLAVE_ADDR 0x69
 #define MAX_PACKET_SIZE 3
-
+//------------------------Clock Definitions for 24 MHz-------------------------------------
 #define CSKEY   0xA500              // Key to unlock CS registers (whole CSCTL0 register)
 #define DCOFSEL_5   0x05            // 24 MHz (nominal)
 #define SELM__DCOCLK 0x0100         // MCLK source select DCOCLK (bits 8-6)
 #define SELS__DCOCLK 0x0010         // SMCLK source select DCOCLK (bits 4-2)
 #define SELA__REFOCLK 0x0000        // ACLK source select REFOCLK (default)
 
+//---------------------Functions-------------------------------------------------
 void rgb_controller_init(void);
 
 
